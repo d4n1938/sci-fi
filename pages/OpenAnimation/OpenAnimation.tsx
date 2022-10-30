c6156891fa56ecbeb9a9ca6175eda5484366202a
Lnida <91710828+d4n1938@users.noreply.github.com>
2022-10-30 23:45:27 +0900

    type green

--- pages/OpenAnimation/OpenAnimation.tsx
+++ pages/OpenAnimation/OpenAnimation.tsx
@@ -1,14 +1,39 @@
+import gsap from "gsap";
 import { useEffectOnce } from "usehooks-ts";
 
 const OpenAnimation = () => {
-  useEffectOnce(() => {});
+  useEffectOnce(() => {
+    var tl = gsap.timeline({ repeat: -1 });
+    tl.to(".writeLine:nth-child(1)", {
+      duration: 0.7,
+      borderColor: "rgba(0, 255, 51, 0)",
+    });
+    gsap.to(".writeLine:nth-child(1)", {
+      width: 1000,
+      duration: 0.8,
+      ease: "none",
+    });
+    gsap.to(".writeLine:nth-child(2)", {
+      width: 1000,
+      duration: 0.8,
+      ease: "none",
+    });
+  });
   return (
     <>
       <section>
-        <p className="line-1 anim-typewriter">
+        {/* <p className="line-1 anim-typewriter">
           Animation typewriter style using css steps()
         </p>
-        <div id="typedtext"></div>
+        <div id="typedtext"></div> */}
+        <div className="wrapper">
+          <div className="writeLine">
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </div>{" "}
+          <div className="writeLine">
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </div>
+        </div>
       </section>
       {/* style--------------------------------------------------- */}
       <style jsx>{`
@@ -20,7 +45,20 @@ const OpenAnimation = () => {
           @include flex-center(100vw, 100vh);
           background-color: $mainGray;
           div {
-            color: white;
+            font-size: 3rem;
+            color: $textGreen;
+          }
+          .wrapper {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            .writeLine {
+              top: 0;
+              left: 20px;
+              border-right: 10px solid rgba(0, 255, 51, 1);
+              overflow: hidden;
+              width: 0;
+            }
           }
         }
       `}</style>
