b46af5c0d4bf2686fae13464d1e673aff9136ceb
Lnida <91710828+d4n1938@users.noreply.github.com>
2022-10-31 16:13:40 +0900

    typing animation complete

--- pages/OpenAnimation/OpenAnimation.tsx
+++ pages/OpenAnimation/OpenAnimation.tsx
@@ -5,18 +5,12 @@ import { useEffectOnce } from "usehooks-ts";
 const OpenAnimation = () => {
   const date: Date = new Date();
   const time = String(date);
-  console.log(time);
   useEffectOnce(() => {
-    // var tl = gsap.timeline({ repeat: -1 });
-    // tl.to(".writeLine:nth-child(1)", {
-    //   duration: 0.7,
-    //   borderColor: "rgba(0, 255, 51, 0)",
-    // });
     var writeTl = gsap.timeline({});
 
     writeTl
       .to(".writeLine:nth-child(1)", {
-        width: "28rem",
+        width: "25rem",
         duration: 0.3,
         ease: "none",
         onComplete: () => {
@@ -27,27 +21,99 @@ const OpenAnimation = () => {
         },
       })
       .to(".writeLine:nth-child(2)", {
-        width: "99rem",
+        width: "95rem",
         duration: 0.8,
         ease: "none",
         delay: 1.8,
+      })
+      .to(".writeLine:nth-child(3)", {
+        width: 360,
+        duration: 0.4,
+        ease: "none",
+        onComplete: () => {
+          gsap.timeline({ repeat: 3 }).to(".writeLine:nth-child(3)", {
+            duration: 0.5,
+            borderColor: "rgba(0, 255, 51, 0)",
+          });
+        },
+      })
+      .to(".writeLine:nth-child(4)", {
+        width: 360,
+        duration: 0,
+        ease: "none",
+        delay: 2.2,
+      })
+      .to(".writeLine:nth-child(5)", {
+        width: 360,
+        duration: 0,
+        ease: "none",
+        delay: 0.8,
+      })
+      .to(".writeLine:nth-child(6)", {
+        width: 360,
+        duration: 0,
+        ease: "none",
+        delay: 0.4,
+      })
+      .to(".writeLine:nth-child(7)", {
+        width: 360,
+        duration: 0,
+        ease: "none",
+        delay: 0.4,
+      })
+      .to(".writeLine:nth-child(8)", {
+        width: 360,
+        duration: 0,
+        ease: "none",
+        delay: 0.2,
+      })
+      .to(".writeLine:nth-child(9)", {
+        width: 750,
+        duration: 0.6,
+        ease: "none",
+        delay: 0.6,
+        onComplete: () => {
+          gsap.timeline({ repeat: 3 }).to(".writeLine:nth-child(9)", {
+            duration: 0.5,
+            borderColor: "rgba(0, 255, 51, 0)",
+          });
+        },
+      })
+      .to(".writeLine:nth-child(10)", {
+        width: 750,
+        duration: 0.6,
+        ease: "none",
+        delay: 2.2,
+      })
+      .to(".wrapper", {
+        // backgroundColor: "black",
+        // opacity: 0,
+        delay: 1,
+
+        opacity: 0,
+        duration: 1,
       });
   });
   return (
     <>
       <section>
-        {/* <p className="line-1 anim-typewriter">
-          Animation typewriter style using css steps()
-        </p>
-        <div id="typedtext"></div> */}
         <div className="wrapper">
           <div className="writeLine">System booting</div>
           <div className="writeLine">{time}</div>
+          <div className="writeLine">Initializing . . .</div>
+          <div className="writeLine">Level -----1</div>
+          <div className="writeLine">Level -----2</div>
+          <div className="writeLine">Level -----3</div>
+          <div className="writeLine">Level -----4</div>
+          <div className="writeLine">Level -----5</div>
+          <div className="writeLine">Booting sequence all succeed</div>
+          <div className="writeLine">Welcome</div>
         </div>
       </section>
       {/* style--------------------------------------------------- */}
       <style jsx>{`
         @import "../../styles/functions.scss";
+        @import url("https://fonts.googleapis.com/css2?family=Goldman&display=swap");
         section {
           position: fixed;
           top: 0;
@@ -66,6 +132,8 @@ const OpenAnimation = () => {
               font-size: 3rem;
               color: $textGreen;
               white-space: nowrap;
+              font-family: "Maven Pro", sans-serif;
+              font-family: "Goldman", cursive;
               border-right: 10px solid rgba(0, 255, 51, 0);
               overflow: hidden;
               width: 0;
