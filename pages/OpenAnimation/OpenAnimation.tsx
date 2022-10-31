673727e7a2d5fd01c621e3da356799aae398bc55
Lnida <91710828+d4n1938@users.noreply.github.com>
2022-10-31 15:24:33 +0900

    add time

--- pages/OpenAnimation/OpenAnimation.tsx
+++ pages/OpenAnimation/OpenAnimation.tsx
@@ -1,23 +1,37 @@
+import { delay } from "framer-motion";
 import gsap from "gsap";
 import { useEffectOnce } from "usehooks-ts";
 
 const OpenAnimation = () => {
+  const date: Date = new Date();
+  const time = String(date);
+  console.log(time);
   useEffectOnce(() => {
-    var tl = gsap.timeline({ repeat: -1 });
-    tl.to(".writeLine:nth-child(1)", {
-      duration: 0.7,
-      borderColor: "rgba(0, 255, 51, 0)",
-    });
-    gsap.to(".writeLine:nth-child(1)", {
-      width: 1000,
-      duration: 0.8,
-      ease: "none",
-    });
-    gsap.to(".writeLine:nth-child(2)", {
-      width: 1000,
-      duration: 0.8,
-      ease: "none",
-    });
+    // var tl = gsap.timeline({ repeat: -1 });
+    // tl.to(".writeLine:nth-child(1)", {
+    //   duration: 0.7,
+    //   borderColor: "rgba(0, 255, 51, 0)",
+    // });
+    var writeTl = gsap.timeline({});
+
+    writeTl
+      .to(".writeLine:nth-child(1)", {
+        width: "28rem",
+        duration: 0.3,
+        ease: "none",
+        onComplete: () => {
+          gsap.timeline({ repeat: 3 }).to(".writeLine:nth-child(1)", {
+            duration: 0.5,
+            borderColor: "rgba(0, 255, 51, 0)",
+          });
+        },
+      })
+      .to(".writeLine:nth-child(2)", {
+        width: "99rem",
+        duration: 0.8,
+        ease: "none",
+        delay: 1.8,
+      });
   });
   return (
     <>
@@ -27,12 +41,8 @@ const OpenAnimation = () => {
         </p>
         <div id="typedtext"></div> */}
         <div className="wrapper">
-          <div className="writeLine">
-            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          </div>{" "}
-          <div className="writeLine">
-            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          </div>
+          <div className="writeLine">System booting</div>
+          <div className="writeLine">{time}</div>
         </div>
       </section>
       {/* style--------------------------------------------------- */}
@@ -44,18 +54,19 @@ const OpenAnimation = () => {
           left: 0;
           @include flex-center(100vw, 100vh);
           background-color: $mainGray;
-          div {
-            font-size: 3rem;
-            color: $textGreen;
-          }
+
           .wrapper {
             position: relative;
+            margin: 40px;
             width: 100%;
             height: 100%;
             .writeLine {
               top: 0;
               left: 20px;
-              border-right: 10px solid rgba(0, 255, 51, 1);
+              font-size: 3rem;
+              color: $textGreen;
+              white-space: nowrap;
+              border-right: 10px solid rgba(0, 255, 51, 0);
               overflow: hidden;
               width: 0;
             }
